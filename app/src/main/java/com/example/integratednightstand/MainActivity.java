ae15d5401970dd2c8e8f7fb61778293b3a471faf
Linda Fan <linda.fan.4556@gmail.com>
2022-10-18 12:08:37 -0700

    first draft of dropdown widgets

--- app/src/main/java/com/example/integratednightstand/MainActivity.java
+++ app/src/main/java/com/example/integratednightstand/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.integratednightstand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner hourStart = findViewById(R.id.hourStart);
+        Spinner hourEnd = findViewById(R.id.hourEnd);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hour, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hourStart.setAdapter(adapter);
+        hourStart.setOnItemSelectedListener(this);
+        hourEnd.setAdapter(adapter);
+        hourEnd.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
