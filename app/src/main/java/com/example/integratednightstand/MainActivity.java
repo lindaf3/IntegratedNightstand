37be1e299f33d495ef5f460005249d7f29fc4a26
Linda Fan <linda.fan.4556@gmail.com>
2022-10-18 17:04:37 -0700

    time picker created

--- app/src/main/java/com/example/integratednightstand/MainActivity.java
+++ app/src/main/java/com/example/integratednightstand/MainActivity.java
@@ -2,38 +2,61 @@ package com.example.integratednightstand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.sql.Time;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+
+    Button alarmStart;
+    int startHour, startMinute;
+    Button alarmEnd;
+    int endHour, endMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner hourStart = findViewById(R.id.hourStart);
-        Spinner hourEnd = findViewById(R.id.hourEnd);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hour, android.R.layout.simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        hourStart.setAdapter(adapter);
-        hourStart.setOnItemSelectedListener(this);
-        hourEnd.setAdapter(adapter);
-        hourEnd.setOnItemSelectedListener(this);
+        alarmStart = findViewById(R.id.alarmStart);
+        alarmEnd = findViewById(R.id.alarmEnd);
+
+
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
+    public void alarmEndSetter(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, mins) -> {
+            endHour = hr;
+            endMinute = mins;
+            alarmEnd.setText(String.format(Locale.getDefault(), "%02d:%02d", endHour, endMinute));
+        };
+        int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, endHour, endMinute, false);
+        alarmDialog.setTitle("Select Alarm End Time");
+        alarmDialog.show();
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    }
+    public void alarmStartSetter(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, mins) -> {
+            startHour = hr;
+            startMinute = mins;
+            alarmStart.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMinute));
+        };
+        int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, startHour, startMinute, false);
+
+
+        alarmDialog.setTitle("Select Alarm Start Time");
+        alarmDialog.show();
 
     }
 }
