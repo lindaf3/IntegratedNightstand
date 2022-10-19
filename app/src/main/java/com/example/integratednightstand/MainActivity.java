a5fddce680a7751aec52f34e9d3125d82a140edc
Linda Fan <linda.fan.4556@gmail.com>
2022-10-18 22:50:39 -0700

    added toast

--- app/src/main/java/com/example/integratednightstand/MainActivity.java
+++ app/src/main/java/com/example/integratednightstand/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.integratednightstand;
 
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button alarmStart;
     int startHour, startMinute;
     Button alarmEnd;
+    Button alarmSet;
     int endHour, endMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,42 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         alarmStart = findViewById(R.id.alarmStart);
         alarmEnd = findViewById(R.id.alarmEnd);
+        alarmSet = findViewById(R.id.alarmSetter);
+
+        alarmSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Alarm Set!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alarmStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, min) -> {
+                    startHour = hr;
+                    startMinute = min;
+                    alarmStart.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMinute));
+                };
+                int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+                TimePickerDialog alarmDialog = new TimePickerDialog(view.getContext(), style, onTimeSetListener, startHour, startMinute, true);
+
+
+                alarmDialog.setTitle("Select Alarm Start Time");
+                alarmDialog.show();
+            }
+        });
+        alarmEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, min) -> {
+                    endHour = hr;
+                    endMinute = min;
+                    alarmEnd.setText(String.format(Locale.getDefault(), "%02d:%02d", endHour, endMinute));
+                };
+                int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+                TimePickerDialog alarmDialog = new TimePickerDialog(view.getContext(), style, onTimeSetListener, endHour, endMinute, true);
+                alarmDialog.setTitle("Select Alarm End Time");
+                alarmDialog.show();
+            }
+        });
 
 
     }
 
-
-//    public void test(View view) {
-//        MaterialTimePicker startAlarm = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H)
-//                .setHour(12)
-//                .setTitleText("Select Appointment time")
-//                .build();
-//        startAlarm.show(fragmentManager, "tag");
-//    }
-    public void alarmEndSetter(View view) {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, mins) -> {
-            endHour = hr;
-            endMinute = mins;
-            alarmEnd.setText(String.format(Locale.getDefault(), "%02d:%02d", endHour, endMinute));
-        };
-        int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
-        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, endHour, endMinute, true);
-        alarmDialog.setTitle("Select Alarm End Time");
-        alarmDialog.show();
-
-    }
-    public void alarmStartSetter(View view) {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, mins) -> {
-            startHour = hr;
-            startMinute = mins;
-            alarmStart.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMinute));
-        };
-        int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
-        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, startHour, startMinute, true);
-
-
-        alarmDialog.setTitle("Select Alarm Start Time");
-        alarmDialog.show();
-
-    }
 }
