135bc33270cc5f1ff6f944220e790206ce0934e8
Linda Fan <linda.fan.4556@gmail.com>
2022-10-18 17:28:43 -0700

    24 hour clock for now

--- app/src/main/java/com/example/integratednightstand/MainActivity.java
+++ app/src/main/java/com/example/integratednightstand/MainActivity.java
@@ -1,19 +1,16 @@
 package com.example.integratednightstand;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
-import java.sql.Time;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+//    public void test(View view) {
+//        MaterialTimePicker startAlarm = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H)
+//                .setHour(12)
+//                .setTitleText("Select Appointment time")
+//                .build();
+//        startAlarm.show(fragmentManager, "tag");
+//    }
     public void alarmEndSetter(View view) {
         TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, hr, mins) -> {
             endHour = hr;
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             alarmEnd.setText(String.format(Locale.getDefault(), "%02d:%02d", endHour, endMinute));
         };
         int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
-        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, endHour, endMinute, false);
+        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, endHour, endMinute, true);
         alarmDialog.setTitle("Select Alarm End Time");
         alarmDialog.show();
 
@@ -52,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             alarmStart.setText(String.format(Locale.getDefault(), "%02d:%02d", startHour, startMinute));
         };
         int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
-        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, startHour, startMinute, false);
+        TimePickerDialog alarmDialog = new TimePickerDialog(this, style, onTimeSetListener, startHour, startMinute, true);
 
 
         alarmDialog.setTitle("Select Alarm Start Time");
